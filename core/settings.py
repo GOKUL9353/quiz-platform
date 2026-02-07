24acb080c25a1d6e46f01cbe5318c1f663c57fdd
Gokul <gokulsrini820@gmail.com>
2026-02-07 19:12:18 +0530

    Updated features and bug fixes

--- core/settings.py
+++ core/settings.py
@@ -15,9 +15,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.environ.get('SECRET_KEY', 'django-insecure-tf)a)p#cyzci4nj3sh6u_0rv3h_7zjwy%)qf4!fzgepf=%9z99')
 
 # Debug mode - set via environment variable
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = ['.onrender.com']
 
 
 # Application definition
